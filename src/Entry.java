3ce2ef27d77602da262016c85385c737a43a71a4
Haritha Chitta <haritha.chitta@gmail.com>
2022-05-19 14:32:58 +0530

    add new method

--- src/Entry.java
+++ src/Entry.java
@@ -3,4 +3,10 @@ public class Entry {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
+
+    private static void getData(String str) {
+        if("Java Concepts".equalsIgnoreCase(str)) {
+            System.out.println("Welcome");
+        }
+    }
 }
